d43b317fb632f3f6a6f37ef6f4f1f7b0bc7f8d93
florian lahitte <lahitte.florian@gmail.com>
2016-11-07 18:16:05 +0100

    Add 02

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'game' => 'wordgame#game'
+  get 'score' => 'wordgame#score'
+  root 'wordgame#game'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
